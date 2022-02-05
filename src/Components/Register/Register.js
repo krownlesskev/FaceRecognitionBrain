098ac162577ec44cfc16ccf7ebb9687d9821ffb6
Kevin Sana <kevin.sana@outlook.com>
2022-02-04 23:01:36 -0600

    changed endpoint

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://quiet-anchorage-75334.herokuapp.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
