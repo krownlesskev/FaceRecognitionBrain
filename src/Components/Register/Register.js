f338e3a179a4198d2788a27064e7504d4785ac8c
Kevin Sana <kevin.sana@outlook.com>
2022-02-04 23:23:27 -0600

    troubleshooting cors issue

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -33,7 +33,7 @@ class Register extends React.Component {
             .then(user => {
                 if (user.id) {
                     this.props.loadUser(user)
-                    this.props.onRouteChange('home')
+                    this.props.onRouteChange('/')
                 }
             })
 
